67fca2b3ae31f70c58b823faf347b94dc7613f6b
shelling <navyblueshellingford@gmail.com>
2010-01-16 19:48:28 +0800

    add mode specification to shebang insertion

--- .emacs.d/convention/shebang.el
+++ .emacs.d/convention/shebang.el
@@ -1,13 +1,13 @@
 (defun insert-shebang-perl ()
   (interactive)
-  (insert "#!/usr/bin/env perl\n\nuse warnings;\nuse strict;\n\n"))
+  (insert "#!/usr/bin/env perl\n#-*- mode: cperl -*-\n\nuse warnings;\nuse strict;\n\n"))
 
 (defun insert-shebang-ruby ()
   (interactive)
-  (insert "#!/usr/bin/env ruby\n\nrequire 'rubygems'\n\n"))
+  (insert "#!/usr/bin/env ruby\n#-*- mode: ruby -*-\n\nrequire 'rubygems'\n\n"))
 
 (defun insert-shebang-python ()
   (interactive)
-  (insert "#!/usr/bin/env python\n\n"))
+  (insert "#!/usr/bin/env python\n#-*- mode: python -*-\n\n"))
 
 (provide 'shebang)
