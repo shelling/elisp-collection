b03e3383a76ed7f9fb789bf952568726fe9130fb
shelling <navyblueshellingford@gmail.com>
2010-01-17 23:28:19 +0800

    more keybinding

--- .emacs.d/etc/binding.el
+++ .emacs.d/etc/binding.el
@@ -1,19 +1,24 @@
 ;; binding.el
-;;   Set all global key binding here.
+;;   Set all custom key binding here.
 ;;
 
 
 ;; FOR DEFINED IN etc/defun.el
 (define-key global-map "\C-j" 'join-line-below)
+(define-key ctl-x-map "j" 'join-line-below)
+(define-key ctl-x-map "c" 'recompile-emacs.d)
 ;;(define-key global-map "\C-xk" 'kill-buffer-and-frame)
+
 
 ;; FOR CONVENNIENT
 (define-key global-map "\M-!" 'eshell)
+
 
 ;; FOR PLUGINS
 (define-key global-map "\C-xl" 'wb-line-number-toggle)
 (global-set-key "\M-z" 'undo)
 (global-set-key "\M-Z" 'redo)
 
+
 ;;
-(define-key global-map "\M-RET" 'dired-find-file-other-window)
+;; (define-key global-map "\M-RET" 'dired-find-file-other-window)
