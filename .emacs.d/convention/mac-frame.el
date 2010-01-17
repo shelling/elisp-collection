d2bfaa835e3609891ed2cf2ba536a516a38ac039
shelling <navyblueshellingford@gmail.com>
2010-01-17 23:39:34 +0800

    rewrite frame manipulating function

--- .emacs.d/convention/mac-frame.el
+++ .emacs.d/convention/mac-frame.el
@@ -7,8 +7,13 @@
 
 (defun mac-frame-default ()
   (interactive)
-  (set-frame-height nil frame-default-height)
-  (set-frame-width nil frame-default-width))
+;;   (set-frame-position nil
+;;                       (cdr (assoc 'left frame-setting-mbp))
+;;                       (cdr (assoc 'top frame-setting-mbp)))
+  (set-frame-height nil
+                    (cdr (assoc 'height frame-setting-mbp)))
+  (set-frame-width nil
+                   (cdr (assoc 'width frame-setting-mbp))))
 
 
 (provide 'mac-frame)
