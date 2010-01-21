63349cca89726ac6f33bb5f56a99d147abd5d440
shelling <navyblueshellingford@gmail.com>
2010-01-21 18:31:41 +0800

    using require mechanism in etc.el

--- .emacs.d/etc.el
+++ .emacs.d/etc.el
@@ -5,15 +5,15 @@
 (add-to-list 'load-path
              (expand-file-name "~/.emacs.d/etc/"))
 
-(require 'self-custom)
-(require 'defun)
-(require 'binding)
-(require 'auto-mode)
+(require 'self-custom);; personal setting
+(require 'defun)      ;; misc personal function
+(require 'binding)    ;; keybinding
+(require 'auto-mode)  ;; more relationshup between extensions filename and major mode
 
-;; (load-file "~/.emacs.d/etc/custom.el")  ;; personal setting
-;; (load-file "~/.emacs.d/etc/defun.el")   ;; misc personal function
-;; (load-file "~/.emacs.d/etc/binding.el") ;; keybinding
-;; (load-file "~/.emacs.d/etc/auto-mode.el") ;; more relationshup between extensions filename and major mode
+;; (load-file "~/.emacs.d/etc/custom.el")
+;; (load-file "~/.emacs.d/etc/defun.el")
+;; (load-file "~/.emacs.d/etc/binding.el")
+;; (load-file "~/.emacs.d/etc/auto-mode.el")
 
 ;; provide symbol
 (provide 'etc)
