9636c65aad3ae8bcf2b41a7f2958beffdd680808
shelling <navyblueshellingford@gmail.com>
2010-01-21 12:34:58 +0800

    set coding pair triggered by C-o

--- .emacs.d/convention/coding-pair.el
+++ .emacs.d/convention/coding-pair.el
@@ -14,13 +14,17 @@
 ;;; this script also include erb syntax <%=  %> and <%  %>
 ;;;
 
+;; undefine C-o
+(define-key global-map "\C-o" nil)
 
 ;; quit coding pair
+;; not completed
 (defun exit-coding-pair ()
   (interactive)
   (forward-char))
 
 (define-key global-map "\M-pl" 'exit-coding-pair)
+
 
 ;; insert ()
 (defun insert-parenthesis ()
@@ -28,7 +32,8 @@
   (insert "()")
   (backward-char))
 
-(define-key global-map "\M-po" 'insert-parenthesis)
+;; (define-key global-map "\M-po" 'insert-parenthesis)
+(define-key global-map "\C-oo" 'insert-parenthesis)
 
 ;; insert {}
 (defun insert-brace ()
@@ -36,7 +41,8 @@
   (insert "{}")
   (backward-char))
 
-(define-key global-map "\M-pu" 'insert-brace)
+;; (define-key global-map "\M-pu" 'insert-brace)
+(define-key global-map "\C-ou" 'insert-brace)
 
 ;; insert ||
 (defun insert-parallel ()
@@ -44,7 +50,8 @@
   (insert "||")
   (backward-char))
 
-(define-key global-map "\M-pi" 'insert-parallel)
+;; (define-key global-map "\M-pi" 'insert-parallel)
+(define-key global-map "\C-oi" 'insert-parallel)
 
 ;; insert ""
 (defun insert-double-quote ()
@@ -52,7 +59,8 @@
   (insert "\"\"")
   (backward-char))
 
-(define-key global-map "\M-pd" 'insert-double-quote)
+;; (define-key global-map "\M-pd" 'insert-double-quote)
+(define-key global-map "\C-od" 'insert-double-quote)
 
 ;; insert ''
 (defun insert-single-quote ()
@@ -60,28 +68,32 @@
   (insert "''")
   (backward-char))
 
-(define-key global-map "\M-ps" 'insert-single-quote)
+;; (define-key global-map "\M-ps" 'insert-single-quote)
+(define-key global-map "\C-os" 'insert-single-quote)
 
 ;; insert ->
 (defun insert-reference ()
   (interactive)
   (insert "->"))
 
-(define-key global-map "\M-pr" 'insert-reference)
+;; (define-key global-map "\M-pr" 'insert-reference)
+(define-key global-map "\C-or" 'insert-reference)
 
 ;; insert =>
 (defun insert-mapping ()
   (interactive)
   (insert "=>"))
 
-(define-key global-map "\M-pm" 'insert-mapping)
+;; (define-key global-map "\M-pm" 'insert-mapping)
+(define-key global-map "\C-om" 'insert-mapping)
 
 ;; insert ::
 (defun insert-namespace-separator ()
   (interactive)
   (insert "::"))
 
-(define-key global-map "\M-pn" 'insert-namespace-separator)
+;; (define-key global-map "\M-pn" 'insert-namespace-separator)
+(define-key global-map "\C-on" 'insert-namespace-separator)
 
 ;; insert <>
 (defun insert-tag-pair ()
@@ -89,7 +101,8 @@
   (insert "<>")
   (backward-char))
 
-(define-key global-map "\M-pv" 'insert-tag-pair)
+;; (define-key global-map "\M-pv" 'insert-tag-pair)
+(define-key global-map "\C-ov" 'insert-tag-pair)
 
 ;; insert <%=  %>
 (defun insert-erb-eval ()
@@ -97,7 +110,8 @@
   (insert "<%=  %>")
   (backward-char 3))
 
-(define-key global-map "\M-p5" 'insert-erb-eval)
+;; (define-key global-map "\M-p5" 'insert-erb-eval)
+(define-key global-map "\C-o5" 'insert-erb-eval)
 
 ;; insert <%  %>
 (defun insert-erb-tag ()
@@ -105,7 +119,8 @@
   (insert "<%  %>")
   (backward-char 3))
 
-(define-key global-map "\M-p%" 'insert-erb-tag)
+;; (define-key global-map "\M-p%" 'insert-erb-tag)
+(define-key global-map "\C-o%" 'insert-erb-tag)
 
 ;; insert []
 (defun insert-key-tag ()
@@ -113,7 +128,8 @@
   (insert "[]")
   (backward-char))
 
-(define-key global-map "\M-pk" 'insert-key-tag)
+;; (define-key global-map "\M-pk" 'insert-key-tag)
+(define-key global-map "\C-ok" 'insert-key-tag)
 
 
 (provide 'coding-pair)
