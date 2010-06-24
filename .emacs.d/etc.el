49ee8a072107c7da338e57d28cd8025a2b8d30ee
shelling <navyblueshellingford@gmail.com>
2010-06-25 01:19:37 +0800

    remote deprecated line in etc.el

--- .emacs.d/etc.el
+++ .emacs.d/etc.el
@@ -2,18 +2,12 @@
 ;;   Load all file in ~/.emacs.d/etc/
 ;;
 
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/etc/"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/etc/"))
 
 (require 'self-custom);; personal setting
 (require 'defun)      ;; misc personal function
 (require 'binding)    ;; keybinding
 (require 'auto-mode)  ;; more relationshup between extensions filename and major mode
-
-;; (load-file "~/.emacs.d/etc/custom.el")
-;; (load-file "~/.emacs.d/etc/defun.el")
-;; (load-file "~/.emacs.d/etc/binding.el")
-;; (load-file "~/.emacs.d/etc/auto-mode.el")
 
 ;; provide symbol
 (provide 'etc)
