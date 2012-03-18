5439fe3319672754ac083fe2ad366e61d7574d10
shelling <navyblueshellingford@gmail.com>
2012-03-18 18:18:47 +0800

    * add window resizing keybindings

--- .emacs.d/etc/binding.el
+++ .emacs.d/etc/binding.el
@@ -32,6 +32,15 @@
 (global-set-key (kbd "C-x r r")
                 'replace-rectangle)
 
+;; window resizing 
+(global-set-key (kbd "S-C-<left>")
+                'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>")
+                'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>")
+                'shrink-window)
+(global-set-key (kbd "S-C-<up>")
+                'enlarge-window)
 
 ;; for Carbon Emacs 22
 (if (and (<= emacs-major-version 22) (eq window-system 'mac))
