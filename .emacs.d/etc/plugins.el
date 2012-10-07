eade64feb7ac3e18673f231b3d0fdbe090a5bbae
shelling <navyblueshellingford@gmail.com>
2012-10-07 12:24:15 +0800

    remove some plugin no longer used

--- .emacs.d/etc/plugins.el
+++ .emacs.d/etc/plugins.el
@@ -4,17 +4,8 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins/"))
 
 (require 'redo)                         ; need key mapping
-(require 'run-guile)
-(require 'rect-mark)
 
-
-(require 'vc-git)                  ;; emacs-git
-(require 'snippet)                 ;; emacs-rails
-(require 'find-recursive)          ;; emacs-rails
-(require 'square-braces-as-parens) ;; run-guile
-(require 'set-keys)                ;; run-guile
-(require 'map-table)               ;; run-guile
-(require 'sym-comp)                ;; python.el in emacs 22
+(autoload 'rm-set-mark "rect-mark" "" t nil)
 
 (require 'mercurial)
 (require 'vc-hg)
@@ -22,18 +13,12 @@
 (require 'sr-speedbar)
 
 (require 'highlight-parentheses)
-(require 'autopair )
+(require 'autopair)
 
 (require 'pod-mode)
 
 (require 'folding)
 
-
-;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins/emacs-rails/"))
-;; (require 'rails)
-;; ;; Depend on:
-;; ;;   snippet.el
-;; ;;   find-recursive.el
 
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins/yasnippet-0.6.1c/"))
 (require 'yasnippet)
