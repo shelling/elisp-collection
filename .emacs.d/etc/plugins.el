9f302eb6b9b06980ff2bbd04d765f4f292eb7a5c
shelling <navyblueshellingford@gmail.com>
2012-10-08 13:15:36 +0800

    use redo+.el in ELPA

--- .emacs.d/etc/plugins.el
+++ .emacs.d/etc/plugins.el
@@ -3,7 +3,7 @@
 
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins/"))
 
-(require 'redo)                         ; need key mapping
+(require 'redo+)                         ; need key mapping
 
 (autoload 'rm-set-mark "rect-mark" "" t nil)
 
