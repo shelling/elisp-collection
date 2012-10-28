4f5c3da72ac54483639d5371fc012846972952be
shelling <navyblueshellingford@gmail.com>
2012-10-28 18:02:01 +0800

    rearrange binding. unset C-_ to force using S-z

--- .emacs.d/etc/binding.el
+++ .emacs.d/etc/binding.el
@@ -2,65 +2,45 @@
 ;;   Set all custom key binding here.
 ;;
 
-(global-unset-key [(ctrl ?\/)])      ;; unset C-/
-
+(global-unset-key (kbd "C-/"))       ;; unset C-/
+(global-unset-key (kbd "C-_"))       ;; unset C-_
 
 ;; global convention
-(global-set-key [(super Z)] 'redo)
-(global-set-key [(super z)] 'undo)
+(global-set-key [(super Z)]             'redo)
+(global-set-key [(super z)]             'undo)
 
 ;; windows jumping
-(global-set-key "\M-[" 
-                'previous-multiframe-window)
-(global-set-key "\M-]" 
-                'next-multiframe-window)
+(global-set-key (kbd "M-[")             'previous-multiframe-window)
+(global-set-key (kbd "M-]")             'next-multiframe-window)
 
 ;; line manipulating
-(global-set-key (kbd "M-k")
-                'backward-kill-line)
-(global-set-key (kbd "C-x j")
-                'join-line-below)
+(global-set-key (kbd "M-k")             'backward-kill-line)
+(global-set-key (kbd "C-x j")           'join-line-below)
 
-(global-set-key (kbd "C-x p")
-                'sr-speedbar-focus-toggle)
+(global-set-key (kbd "C-x p")           'sr-speedbar-focus-toggle)
 
-(global-set-key (kbd "C-x a a")
-                'align)
-(global-set-key (kbd "C-x a c")
-                'align-current)
-(global-set-key (kbd "C-x a r")
-                'align-regexp)
+(global-set-key (kbd "C-x a a")         'align)
+(global-set-key (kbd "C-x a c")         'align-current)
+(global-set-key (kbd "C-x a r")         'align-regexp)
 
-(global-set-key (kbd "C-c TAB")
-                'folding-toggle-show-hide)
+(global-set-key (kbd "C-c TAB")         'folding-toggle-show-hide)
+(global-set-key (kbd "M-!")             'eshell)
 
-(global-set-key "\M-!" 'eshell)
-
-(define-key ctl-x-map "l" 'global-linum-mode)
-
-(define-key ctl-x-map "c" 'recompile-emacs.d)
+(global-set-key (kbd "C-x l")           'global-linum-mode)
+(global-set-key (kbd "C-x c")           'recompile-emacs.d)
 
 ;; rectangle region editing
-(global-set-key (kbd "C-x r C-SPC")
-                'rm-set-mark)
-(global-set-key (kbd "C-x r C-x")
-                'rm-exchange-point-and-mark)
-(global-set-key (kbd "C-x r C-w")
-                'rm-kill-region)
-(global-set-key (kbd "C-x r M-w")
-                'rm-kill-ring-save)
-(global-set-key (kbd "C-x r r")
-                'replace-rectangle)
+(global-set-key (kbd "C-x r C-SPC")     'rm-set-mark)
+(global-set-key (kbd "C-x r C-x")       'rm-exchange-point-and-mark)
+(global-set-key (kbd "C-x r C-w")       'rm-kill-region)
+(global-set-key (kbd "C-x r M-w")       'rm-kill-ring-save)
+(global-set-key (kbd "C-x r r")         'replace-rectangle)
 
-;; window resizing 
-(global-set-key (kbd "S-C-<left>")
-                'shrink-window-horizontally)
-(global-set-key (kbd "S-C-<right>")
-                'enlarge-window-horizontally)
-(global-set-key (kbd "S-C-<down>")
-                'shrink-window)
-(global-set-key (kbd "S-C-<up>")
-                'enlarge-window)
+;; window resizing
+(global-set-key (kbd "S-C-<left>")      'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>")     'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>")      'shrink-window)
+(global-set-key (kbd "S-C-<up>")        'enlarge-window)
 
 ;; for Cocoa Emacs 23
 (if (and (>= emacs-major-version 23) (eq window-system 'ns))
