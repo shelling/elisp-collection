a8adc3d228d24c8fe64fa7eca491f81dcc2b8597
shelling <navyblueshellingford@gmail.com>
2012-10-28 15:48:16 +0800

    adjust mode-loader

--- .emacs.d/etc/mode.el
+++ .emacs.d/etc/mode.el
@@ -1,69 +1,17 @@
 ;; mode.el
 ;;
 
-(add-to-list
- 'load-path
- (expand-file-name "~/.emacs.d/mode/"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/mode/"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/mode/rhtml-mode/"))
 
-(autoload
-  'cperl-mode
-  "cperl-mode"
-  nil
-  t)
-
-(autoload 
-  'sgml-mode
-  "sgml-mode" 
-  nil
-  t)
-
-(autoload
-  'applescript-mode
-  "applescript-mode"
-  "http://github.com/ieure/applescript-mode"
-  t)
-
-(autoload
-  'erlang-mode
-  "erlang"
-  "http://www.erlang.org/download/contrib/erlang.el"
-  t)
-
-(autoload
-  'yaml-mode
-  "yaml-mode"
-  "YAML Major Mode"
-  t)
-
-(autoload
-  'ruby-mode
-  "ruby-mode"
-  nil
-  t)
-
-(add-to-list
- 'load-path
- (expand-file-name "~/.emacs.d/mode/rhtml-mode/"))
-(autoload
-  'rhtml-mode
-  "rhtml-mode" 
-  "http://github.com/eschulte/rhtml"
-  t)
-
-(autoload
-  'puppet-mode
-  "puppet-mode"
-  "Puppet Major Mode"
-  t)
-
-(autoload
-  'scss-mode
-  "scss-mode"
-  "SCSS Major Mode"
-  t)
-
-(autoload
-  'haml-mode
-  "haml-mode"
-  "HAML Major Mode"
-  t)
+;;        'function         "file"                doc interactive   type(nil, keymap, macro)
+(autoload 'cperl-mode       "cperl-mode"          nil t)
+(autoload 'sgml-mode        "sgml-mode"           nil t)
+(autoload 'applescript-mode "applescript-mode"    nil t)
+(autoload 'erlang-mode      "erlang"              nil t)
+(autoload 'yaml-mode        "yaml-mode"           nil t)
+(autoload 'ruby-mode        "ruby-mode"           nil t)
+(autoload 'rhtml-mode       "rhtml-mode"          nil t)
+(autoload 'puppet-mode      "puppet-mode"         nil t)
+(autoload 'scss-mode        "scss-mode"           nil t)
+(autoload 'haml-mode        "haml-mode"           nil t)
