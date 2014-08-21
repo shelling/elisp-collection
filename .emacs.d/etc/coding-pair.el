a31c876f90af3b287748edf76dbd45bb673d1948
shelling <navyblueshellingford@gmail.com>
2014-08-21 11:37:43 +0800

    Remove function names for inserting pairs

--- .emacs.d/etc/coding-pair.el
+++ .emacs.d/etc/coding-pair.el
@@ -17,117 +17,103 @@
 ;; undefine C-o
 (define-key global-map "\C-o" nil)
 
-;; quit coding pair
-;; not completed
-(defun exit-coding-pair ()
-  (interactive)
-  (forward-char))
-
-(define-key global-map "\M-pl" 'exit-coding-pair)
-
-
 ;; insert ()
-(defun insert-parenthesis ()
-  (interactive)
-  (insert "()")
-  (backward-char))
-
-;; (define-key global-map "\M-po" 'insert-parenthesis)
-(define-key global-map "\C-oo" 'insert-parenthesis)
+(define-key global-map
+  "\C-oo"
+  '(lambda ()
+     (interactive)
+     (insert "()")
+     (backward-char)))
 
 ;; insert {}
-(defun insert-brace ()
-  (interactive)
-  (insert "{}")
-  (backward-char))
-
-;; (define-key global-map "\M-pu" 'insert-brace)
-(define-key global-map "\C-ou" 'insert-brace)
+(define-key global-map
+  "\C-ou"
+  '(lambda ()
+     (interactive)
+     (insert "{}")
+     (backward-char)))
 
 ;; insert ||
-(defun insert-parallel ()
-  (interactive)
-  (insert "||")
-  (backward-char))
-
-;; (define-key global-map "\M-pi" 'insert-parallel)
-(define-key global-map "\C-oi" 'insert-parallel)
+(define-key global-map
+  "\C-oi"
+  '(lambda ()
+     (interactive)
+     (insert "||")
+     (backward-char)))
 
 ;; insert ""
-(defun insert-double-quote ()
-  (interactive)
-  (insert "\"\"")
-  (backward-char))
-
-;; (define-key global-map "\M-pd" 'insert-double-quote)
-(define-key global-map "\C-od" 'insert-double-quote)
+(define-key global-map
+  "\C-od"
+  '(lambda ()
+     (interactive)
+     (insert "\"\"")
+     (backward-char)))
 
 ;; insert ''
-(defun insert-single-quote ()
-  (interactive)
-  (insert "''")
-  (backward-char))
-
-;; (define-key global-map "\M-ps" 'insert-single-quote)
-(define-key global-map "\C-os" 'insert-single-quote)
+(define-key global-map
+  "\C-os"
+  '(lambda ()
+     (interactive)
+     (insert "''")
+     (backward-char)))
 
 ;; insert ->
-(defun insert-reference ()
-  (interactive)
-  (insert "->"))
-
-;; (define-key global-map "\M-pr" 'insert-reference)
-(define-key global-map "\C-or" 'insert-reference)
+(define-key global-map
+  "\C-or"
+  '(lambda ()
+     (interactive)
+     (insert "->")))
 
 ;; insert =>
-(defun insert-mapping ()
-  (interactive)
-  (insert "=>"))
-
-;; (define-key global-map "\M-pm" 'insert-mapping)
-(define-key global-map "\C-om" 'insert-mapping)
+(define-key global-map
+  "\C-om"
+  '(lambda ()
+     (interactive)
+     (insert "=>")))
 
 ;; insert ::
-(defun insert-namespace-separator ()
-  (interactive)
-  (insert "::"))
-
-;; (define-key global-map "\M-pn" 'insert-namespace-separator)
-(define-key global-map "\C-on" 'insert-namespace-separator)
+(define-key global-map
+  "\C-on"
+  '(lambda ()
+     (interactive)
+     (insert "::")))
 
 ;; insert <>
-(defun insert-tag-pair ()
-  (interactive)
-  (insert "<>")
-  (backward-char))
-
-;; (define-key global-map "\M-pv" 'insert-tag-pair)
-(define-key global-map "\C-ov" 'insert-tag-pair)
+(define-key global-map
+  "\C-ov"
+  '(lambda ()
+     (interactive)
+     (insert "<>")
+     (backward-char)))
 
 ;; insert <%=  %>
-(defun insert-erb-eval ()
-  (interactive)
-  (insert "<%=  %>")
-  (backward-char 3))
-
-;; (define-key global-map "\M-p5" 'insert-erb-eval)
-(define-key global-map "\C-o5" 'insert-erb-eval)
+(define-key global-map
+  "\C-o5"
+  '(lambda ()
+     (interactive)
+     (insert "<%=  %>")
+     (backward-char 3)))
 
 ;; insert <%  %>
-(defun insert-erb-tag ()
-  (interactive)
-  (insert "<%  %>")
-  (backward-char 3))
-
-;; (define-key global-map "\M-p%" 'insert-erb-tag)
-(define-key global-map "\C-o%" 'insert-erb-tag)
+(define-key global-map
+  "\C-o%"
+  '(lambda ()
+     (interactive)
+     (insert "<%  %>")
+     (backward-char 3)))
 
 ;; insert []
-(defun insert-key-tag ()
-  (interactive)
-  (insert "[]")
-  (backward-char))
+(define-key global-map
+  "\C-ok"
+  '(lambda ()
+     (interactive)
+     (insert "[]")
+     (backward-char)))
 
-;; (define-key global-map "\M-pk" 'insert-key-tag)
-(define-key global-map "\C-ok" 'insert-key-tag)
-
+;; insert /*  */
+(define-key global-map
+  "\C-o8"
+  '(lambda ()
+     (interactive)
+     (insert "/*  */")
+     (backward-char 3)))
